587f529fbb5d546967bc8a6eabf2a6ef9e871e64
Nawaz <rnbabar@me.com>
2021-09-16 00:10:18 -0700

    Ansible Roboshop Project

--- ansible_direc.sh
+++ ansible_direc.sh
@@ -5,5 +5,10 @@ for i in {0..9}
 do
 echo -e "$(echo -e [${INST[$i]}] | tr [a-z] [A-Z]) \n${INST[$i]}.roboshop.internal">>rb-inv
 done
+#create a Role Diectories /task /var and also main.ymal in tasks and vars
 
+for i in frontend catalogue  mongodb user cart shipping rabbitmq payment mysql redis;
+do 
+mkdir -p $i/tasks $i/vars; touch $i/tasks/main.yml $i/vars/main.yml
+done
 
