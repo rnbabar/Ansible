311ebc04bb1b604a95e4fe2527fed47062add287
Nawaz <rnbabar@me.com>
2021-09-16 00:15:55 -0700

    Ansible Roboshop Project

--- ansible_direc.sh
+++ ansible_direc.sh
@@ -1,14 +1,17 @@
 #! bin/bash
-INST=("frontend" "catalogue"  "mongodb" "user" "cart" "shipping" "rabbitmq" "payment" "mysql" "redis")
+#INST=("frontend" "catalogue"  "mongodb" "user" "cart" "shipping" "rabbitmq" "payment" "mysql" "redis")
 #create a inventory file for instances
-for i in {0..9}
-do
-echo -e "$(echo -e [${INST[$i]}] | tr [a-z] [A-Z]) \n${INST[$i]}.roboshop.internal">>rb-inv
-done
+#for i in {0..9}
+#do
+#echo -e "$(echo -e [${INST[$i]}] | tr [a-z] [A-Z]) \n${INST[$i]}.roboshop.internal">>rb-inv
+#done
+
+
 #create a Role Diectories /task /var and also main.ymal in tasks and vars
 
 for i in frontend catalogue  mongodb user cart shipping rabbitmq payment mysql redis;
-do 
+do
+echo -e "$(echo [$i] | tr [a-z] [A-Z])\n$i.roboshop.internal" >>rb-inv 
 mkdir -p $i/tasks $i/vars; touch $i/tasks/main.yml $i/vars/main.yml
 done
 
