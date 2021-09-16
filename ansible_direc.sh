79d9f997407374fb3c8bfe4d320d80049e5673ba
Nawaz <rnbabar@me.com>
2021-09-15 23:16:19 -0700

    Ansible Roboshop Project

--- ansible_direc.sh
+++ ansible_direc.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 INST=("frontend" "catalogue"  "mongodb" "user" "cart" "shipping" "rabbitmq" "payment" "mysql" "redis")
-for i in {0 .. 9};
+for (i=0; i<10; i++ );
 do
 echo -e "\n IInstance Name =${INST[$i]}"
 done
