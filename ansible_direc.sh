36c1a76325765729fd2b800e90b192bb319ab6ef
Nawaz <rnbabar@me.com>
2021-09-15 23:26:30 -0700

    Ansible Roboshop Project

--- ansible_direc.sh
+++ ansible_direc.sh
@@ -2,6 +2,6 @@
 INST=("frontend" "catalogue"  "mongodb" "user" "cart" "shipping" "rabbitmq" "payment" "mysql" "redis")
 for i in {0..9}
 do
-echo -e "Instance Name =${INST[$i]}"
+echo -e "Instance Name =" tr '[a-z]' '[A-Z]' <${INST[$i]}
 done
 
