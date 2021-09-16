27c0f0f0414204d6eb2731259960e70addaf4dc7
Nawaz <rnbabar@me.com>
2021-09-15 23:47:02 -0700

    Ansible Roboshop Project

--- ansible_direc.sh
+++ ansible_direc.sh
@@ -1,8 +1,8 @@
 #! bin/bash
 INST=("frontend" "catalogue"  "mongodb" "user" "cart" "shipping" "rabbitmq" "payment" "mysql" "redis")
-for i in {0..1}
+for i in {0..9}
 do
-echo -e "[${INST[$i]}] \n ${INST[$i]}.roboshop.internal \n"
+echo -e "[${INST[$i]}]\n${INST[$i]}.roboshop.internal"
 #mkdir  /ansible/${INST[$i]} 
 #mkdir /ansible/${INST[$i]}/tasks
 #touch /ansible/${INST[$i]}/tasks/main.yml
