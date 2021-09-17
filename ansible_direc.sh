a6eb8f57ff4cf0c4b88da2e0eb1d412ecdf6b65f
Nawaz <rnbabar@me.com>
2021-09-16 23:33:14 -0700

    Setting up the instances

--- ansible_direc.sh
+++ ansible_direc.sh
@@ -1,17 +1,9 @@
 #! bin/bash
-#INST=("frontend" "catalogue"  "mongodb" "user" "cart" "shipping" "rabbitmq" "payment" "mysql" "redis")
-#create a inventory file for instances
-#for i in {0..9}
-#do
-#echo -e "$(echo -e [${INST[$i]}] | tr [a-z] [A-Z]) \n${INST[$i]}.roboshop.internal">>rb-inv
-#done
-
-
 #create a Role Diectories /task /var and also main.ymal in tasks and vars
 
 for i in frontend catalogue  mongodb user cart shipping rabbitmq payment mysql redis;
 do
 echo -e "$(echo [$i] | tr [a-z] [A-Z])\n$i.roboshop.internal" >>rb-inv 
-#mkdir -p $i/tasks $i/vars; touch $i/tasks/main.yml $i/vars/main.yml
+mkdir -p $i/tasks $i/vars; touch $i/tasks/main.yml $i/vars/main.yml
 done
 
